57da1aeba181d794c02a9cb1d126b9e7696c8593
Jacob Marshall <me@jhqcat.com>
2024-09-21 12:32:41 +0100

    answer boxes

--- Fundamentals/Hello World/src/Task.java
+++ Fundamentals/Hello World/src/Task.java
@@ -1,6 +1,6 @@
 public class Task {
     public static void main(String[] args) {
-        // Print "Hello World!" on the line below
-
+        // Delete the comment and print "Hello World!" in the box below
+        /* TODO */
     }
 }
